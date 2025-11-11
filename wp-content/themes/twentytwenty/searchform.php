b0a223050d781b3b3761044e3a3fac1c5ae944c3
nguyennguyen777 <hanhnguyenhpt1502@gmail.com>
2025-11-11 21:00:36 +0700

    thêm plugin recent posts widget with thumbnails, tùy chỉnh pages, update functions.php index.php searchform.php style.css

--- wp-content/themes/twentytwenty/searchform.php
+++ wp-content/themes/twentytwenty/searchform.php
@@ -33,7 +33,7 @@ if ( empty( $twentytwenty_aria_label ) && ! empty( $args['label'] ) ) {
 		</span>
 		<div class="search-field-wrapper">
 		<i class="fas fa-search search-icon"></i>
-		<input type="search" id="<?php echo esc_attr( $twentytwenty_unique_id ); ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search topics or keywords', 'placeholder', 'twentytwenty' ); ?>" value="" name="s" />
+		<input type="search" id="<?php echo esc_attr( $twentytwenty_unique_id ); ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search topics or keywords', 'placeholder', 'twentytwenty' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" />
 	</div>
 	</label>
 	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'twentytwenty' ); ?>" />
